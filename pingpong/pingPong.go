bec259bfc0b81d36316d82895507aae7f2c33d7a
SwathiAR <swathi.bhat01@gmail.com>
2016-07-21 12:54:36 -0700

    Done the formatting

--- pingpong/pingPong.go
+++ pingpong/pingPong.go
@@ -3,24 +3,18 @@ package main
 import (
 	"fmt"
 
-
 	"time"
-
 )
 
 var i int = 0;
 
 func main() {
 	ch1 := make(chan string, 10)
-         ch1 <- "Let the game bigin"
-
+	ch1 <- "Let the game bigin"
 
 	go pinger(ch1)
 
-
-
-     time.Sleep(time.Duration(1000000))
-
+	time.Sleep(time.Duration(1000000))
 
 }
 
@@ -36,7 +30,6 @@ func pinger(ch chan string) {
 
 func ponger(ch chan string) {
 	x := <-ch
-
 
 	fmt.Println(x, i)
 	ch <- "pong"
